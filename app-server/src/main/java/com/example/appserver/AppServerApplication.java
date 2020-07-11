b3d11f2baaccc19aec979de9998401fac697bd33
liubinhao <liubinhao@ruigushop.com>
2020-07-11 21:27:55 +0800

    基础配置提交

--- app-server/src/main/java/com/example/appserver/AppServerApplication.java
+++ app-server/src/main/java/com/example/appserver/AppServerApplication.java
@@ -1,9 +1,11 @@
 package com.example.appserver;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.appserver.mapper")
 public class AppServerApplication {
 
     public static void main(String[] args) {
