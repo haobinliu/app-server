21a584e81850cca5f01d533d690a203d59f780d4
liubinhao <liubinhao@ruigushop.com>
2020-07-11 09:46:24 +0800

    项目生成 liubh

---
+++ app-server/src/main/java/com/example/appserver/AppServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.appserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppServerApplication.class, args);
+    }
+
+}
---
+++ app-server/src/test/java/com/example/appserver/AppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
