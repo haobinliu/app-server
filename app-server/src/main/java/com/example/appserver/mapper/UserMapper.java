a967180ca67b2b4faa282322c7b8eb5b96f61750
myname <1300560843@qq.com>
2020-08-02 14:16:30 +0800

    提交

--- app-server/src/main/java/com/example/appserver/mapper/UserMapper.java
+++ app-server/src/main/java/com/example/appserver/mapper/UserMapper.java
@@ -4,7 +4,7 @@ import com.example.appserver.model.entity.User;
 import com.example.appserver.model.req.UserAuthReq;
 
 public interface UserMapper {
-    User checkUserinfo(User user);
+    User checkUserinfo(UserAuthReq userAuthReq);
 
     int insert(User record);
 
